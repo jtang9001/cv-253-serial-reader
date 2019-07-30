b3dbd227e299dad5df8a56d49c9134971f830999
Jiayi Tang <jiayitang10@gmail.com>
2019-07-29 19:03:29 -0700

    made compatible with bp built in LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
 
-#define LED1 PB8
-#define LED2 PB9
+#define LED1 PC13
+//#define LED2 PB9
 #define NUM_CIRCLES 6
-#define CIRCLE_TO_TRACK 0
+#define CIRCLE_TO_TRACK 2
 
 void fillCircleArray(int circleCoords[NUM_CIRCLES][2]);
 void writeCircleToLED(int circleToRead);
@@ -11,15 +11,16 @@ void writeCircleToLED(int circleToRead);
 void setup() {
   Serial.begin(9600);
   pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
+  //pinMode(LED2, OUTPUT);
 }
 
-int circleCoords[NUM_CIRCLES][2];
+int gauntletCoords[NUM_CIRCLES][2];
+int postCoords[2];
 String readValue;
 char readChar;
 
 void loop() {
-  fillCircleArray(circleCoords);
+  fillCircleArray(gauntletCoords);
   
   writeCircleToLED(CIRCLE_TO_TRACK);
 }
@@ -30,28 +31,37 @@ void fillCircleArray(int circleCoords[NUM_CIRCLES][2]) {
     if (readChar == 'G') {
       for (int i = 0; i < NUM_CIRCLES; i++) {
         readValue = Serial.readStringUntil(',');
-        circleCoords[i][0] = readValue.toInt();
+        gauntletCoords[i][0] = readValue.toInt();
         readValue = Serial.readStringUntil(';');
-        circleCoords[i][1] = readValue.toInt();
+        gauntletCoords[i][1] = readValue.toInt();
         // Serial.print(circleCoords[i][0]);
         // Serial.print(' ');
         // Serial.println(circleCoords[i][1]);
       }
     }
+    else if (readChar == 'P') {
+      readValue = Serial.readStringUntil(',');
+      postCoords[0] = readValue.toInt();
+      readValue = Serial.readStringUntil(';');
+      postCoords[1] = readValue.toInt();
+      // Serial.print(circleCoords[i][0]);
+      // Serial.print(' ');
+      // Serial.println(circleCoords[i][1]);
+    }
   }
 }
 
 void writeCircleToLED(int circleToRead) {
-  if (circleCoords[circleToRead][0] < 0) {
+  if (gauntletCoords[circleToRead][0] < 0) {
     digitalWrite(LED1, HIGH);
   }
   else {
     digitalWrite(LED1, LOW);
   }
-  if (circleCoords[circleToRead][1] < 0) {
-    digitalWrite(LED2, HIGH);
-  }
-  else {
-    digitalWrite(LED2, LOW);
-  }
+  // if (circleCoords[circleToRead][1] < 0) {
+  //   digitalWrite(LED2, HIGH);
+  // }
+  // else {
+  //   digitalWrite(LED2, LOW);
+  // }
 }
